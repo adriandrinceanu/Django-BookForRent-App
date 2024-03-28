7baafa45c60c57cbf4a7822548edc9bc11173da4
Adrian Drinceanu <drinceanuadrian@gmail.com>
2024-03-28 15:25:42 +0200

    static file path location fix

--- BookForRent/settings.py
+++ BookForRent/settings.py
@@ -152,9 +152,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/assets/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'BookForRent/static/'),
+    os.path.join(BASE_DIR, '/static/'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'collectstatic')
 # Default primary key field type
