c4f751692cbfd5dd60dbdc5c4e933eb77cd8c300
Adrian Drinceanu <drinceanuadrian@gmail.com>
2024-03-28 15:38:17 +0200

    paths fixed

--- BookForRent/settings.py
+++ BookForRent/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '/static/'),
+    os.path.join(BASE_DIR, 'static/'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'collectstatic')
 # Default primary key field type
