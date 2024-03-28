124373ef79b813c5969e39c52ee3b331a708a6fa
Adrian Drinceanu <drinceanuadrian@gmail.com>
2024-03-28 13:55:34 +0200

    env secret

--- BookForRent/settings.py
+++ BookForRent/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()  # take environment variables from .env.
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
