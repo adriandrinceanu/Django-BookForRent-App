42a8d30fed84248dc6aefccd5ad8b623eb0a474f
Adrian Drinceanu <drinceanuadrian@gmail.com>
2024-03-28 15:51:24 +0200

    csrf fix

--- BookForRent/settings.py
+++ BookForRent/settings.py
@@ -19,11 +19,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()  # take environment variables from .env.
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-l2qt)d83o1gk(*@hw@pa)@%n#xzw*ou00g5zxqzy@$k1crke1f'
+CSRF_TRUSTED_ORIGINS = [
+    'https://django-bookforrent-app.onrender.com/', 
+    'http://localhost:8000', 
+    'http://localhost:80', 
+    'http://127.0.0.1:8000', 
+    'http://127.0.0.1:80', 
+
+    ]
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
