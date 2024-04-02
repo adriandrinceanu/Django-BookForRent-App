37c43b267bb7f9de7033db11510342b200a465b1
Adrian Drinceanu <drinceanuadrian@gmail.com>
2024-04-02 13:45:04 +0300

    gunicorn for media serve

--- BookForRent/docker_entrypoint.sh
+++ BookForRent/docker_entrypoint.sh
@@ -6,4 +6,6 @@ python manage.py migrate
 python manage.py createsuperuser --no-input
 python manage.py collectstatic --no-input
 # python manage.py loaddata db.json
-python manage.py runserver 0.0.0.0:8000
+# python manage.py runserver 0.0.0.0:8000
+
+gunicorn --config gunicorn-cfg.py core.wsgi -b 0.0.0.0:8000
